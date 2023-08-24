40544d4b298b53c94e95558db1ee15d0d7217be2
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-08-23 21:24:59 -0500

    working on it

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -5,17 +5,20 @@ import { useDispatch, useSelector} from 'react-redux';
 function PlantList() {
     const dispatch = useDispatch();
 
-    const reduxState = useSelector(store => store);
+    const reduxState = useSelector(store => store.plantList );
 
     useEffect(() => {
         console.log('component did mount');
         // dispatch an action to request the plantList from the API
+        dispatch({
+            type:'GET_PLANTS'
+        })
     }, []); 
 
     return (
         <div>
             <h3>This is the plant list</h3>
-            <pre>{JSON.stringify(reduxState)}</pre>
+            <h3>{JSON.stringify(reduxState)}</h3>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger'
+import createSageMiddleware from 'redux-saga'
+import {takeLatest, put, take} from 'redux-saga/effects'
+import axios from 'axios';
 
 import App from './App';
 
 // this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+// const startingPlantArray = [
+//   { id: 1, name: 'Rose' },
+//   { id: 2, name: 'Tulip' },
+//   { id: 3, name: 'Oak' }
+// ];
 
-const plantList = (state = startingPlantArray, action) => {
+const sagaMiddleware = createSageMiddleware();
+
+
+
+function* rootSaga(){
+  yield takeLatest('GET_PLANTS', getPlants)
+  yield takeLatest('PLANT', addPlants)
+}
+
+function* addPlants(action){
+  yield axios.post('/api/plant', action.payload)
+  yield put({type:'GET_PLANTS'})
+}
+
+function* getPlants(action){
+  const plants = yield axios.get('/api/plant')
+  console.log('data:', plants.data[0].name)
+  yield put({type:'ADD_PLANT', payload: plants.data})
+}
+
+
+const plantList = (state = 0, action) => {
+  console.log('payload:', action.payload)
   switch (action.type) {
     case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+      return action.payload
     default:
       return state;
   }
 };
 
 const store = createStore(
-  combineReducers({ plantList }),
+  combineReducers({ 
+    plantList 
+  }),
+  applyMiddleware(
+    logger,
+    sagaMiddleware
+  ),
 );
 
+
+sagaMiddleware.run(rootSaga);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
